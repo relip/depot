ac9e1524bc8ecfcaa60895794e17d96aee63a03f
Larry Kim <admin@relip.org>
2014-10-29 13:54:23 +0900

    UI updates; the commit e53b90 was old one

--- app/static/js/upload.js
+++ app/static/js/upload.js
@@ -22,31 +22,41 @@ function makeid()
 }
 
 var browserCurrPath = "/";
-var isLocalFile = false;
+var isRemoteFile = false;
+var existingGroups = [];
+
+window.onpopstate = function(event) {
+	// FIXME: Mobile Safari fires window.onpopstate even on first page load
+	if(event.state == null)
+		return;
+	browserCurrPath = event.state;
+	browse(event.state);
+	console.log("location: " + document.location + ", state: " + JSON.stringify(event.state));
+};
 
 function browse(path)
 {
-	$("#depot-browser-currdir").text(browserCurrPath);
+	$("#browser-currdir").text(browserCurrPath);
 	$.getJSON("/api/browse?path="+path, function(data)
 	{
 		console.log(data);
-		$(".depot-browser-alert").remove();
-		$(".depot-browser-element").remove();
-		$("#depot-browser-tbody").append("<tr class=\"depot-browser-element depot-browser-parent-dir\"><td>D</td><td>..</td><td></td></tr>");
+		$(".browser-alert").remove();
+		$(".browser-element").remove();
+		$("#browser-tbody").append("<tr class=\"browser-element browser-parent-dir\"><td>D</td><td>..</td><td></td></tr>");
 		if(data.result)
 		{
 			$.each(data.data.directories, function(_, fn)
 			{
-				$("#depot-browser-tbody").append("<tr class=\"depot-browser-element\"><td>D</td><td class=\"depot-browser-directory\">"+fn+"</td><td></td></tr>");
+				$("#browser-tbody").append("<tr class=\"browser-element\"><td>D</td><td class=\"browser-directory\">"+fn+"</td><td><button class=\"depot-box browser-upload-all\" style=\"padding: 1px;\" type=\"button\">Upload all</button></td></tr>");
 			});
 			$.each(data.data.files, function(_, fn)
 			{
-				$("#depot-browser-tbody").append("<tr class=\"depot-browser-element\"><td>F</td><td class=\"depot-browser-file\">"+fn.name+"</td><td>"+fn.size+"</td></tr>");
+				$("#browser-tbody").append("<tr class=\"browser-element\"><td>F</td><td class=\"browser-file\">"+fn.name+"</td><td>"+fn.size+"</td></tr>");
 			});
 		}
 		else
 		{
-			$("#depot-browser-tbody").append("<tr><th colspan=\"3\" class=\"depot-browser-alert\">Error: " + data.message + "</th></tr>");
+			$("#browser-tbody").append("<tr><th colspan=\"3\" class=\"browser-alert\">Error: " + data.message + "</th></tr>");
 		}
 	});
 }
@@ -64,7 +74,7 @@ $(function () {
 	{
 		if ($(this).val().length <= 0) { return; }
 
-        	isLocalFile = False;
+        	isRemoteFile = false;
 
 		$("#filenameInput").css({ 'text-align': 'left' });
 		$(".uploadProgress").removeClass("progress-bar-danger");
@@ -74,49 +84,101 @@ $(function () {
         
 		$("#filenameInput").val(filename);
 	});
-   	$(".uploadButton").click(function()
+   	$(document).on("click", ".uploadButton", function()
 	{
-		$(".uploadForm").submit();	
+		if($("#filenameInput").val())
+			$("#uploadForm").submit();
 	});
-	$(".uploadForm").submit(function (e) {
-		upload();
+	$("#uploadForm").submit(function (e)
+	{
+		if (isRemoteFile)
+			upload(browserCurrPath+$("#filenameInput").val());
+		else
+			upload();
 		e.preventDefault();
 	});
-	$("#depot-browse-button").click(function()
+	$("#open-browser").click(function()
 	{
-		if($("#depot-browser").is(":hidden"))
+		if($("#browser").is(":hidden"))
 		{
-			$("#depot-browser").slideDown("slow");
-			$("#depot-browser-tbody").append("<tr><th colspan=\"3\" class=\"depot-browser-alert\">Loading</th></tr>");
+			$(this).text("Close remote file browser");
+			$("#browser").slideDown("fast");
+			$("#browser-tbody").append("<tr><th colspan=\"3\" class=\"browser-alert\">Loading</th></tr>");
+			history.pushState("/", "", "#/");
 			browse("/");
 		}
 		else
 		{
-			$("#depot-browser").hide();
+			$(this).text("Open remote file browser");
+			$("#browser").hide();
 		}
 	});
-	$(document).on("click", ".depot-browser-directory", function()
+
+	$("#create-random-group").click(function()
+	{
+		$.post("/groups/create", undefined, function(data)
+		{
+			$("#group-name").val(data.path);
+		}, 'json');
+		$(this).attr("disabled", "disabled");
+	});
+	$(document).on("click", ".browser-directory", function()
 	{
 		browserCurrPath = browserCurrPath + $(this).text() + "/";
+		history.pushState(browserCurrPath, "", "#"+browserCurrPath);
 		browse(browserCurrPath);
 	});
-	$(document).on("click", ".depot-browser-parent-dir", function()
+	$(document).on("click", ".browser-parent-dir", function()
 	{
+		$("#filenameInput").val("");
 		browserCurrPath = browserCurrPath.split("/").filter(function(a){return a.length != 0;});
 		if(!browserCurrPath)
 		{
 			return;
 		}
 		currPathList = browserCurrPath.splice(0, browserCurrPath.length-1);
+		console.log(currPathList);
 		browserCurrPath = "/" + currPathList.join("/");
-		if(!currPathList)
+		if(currPathList.length != 0)
 			browserCurrPath = browserCurrPath + "/";
+		history.pushState(browserCurrPath, "", "#"+browserCurrPath);
 		browse(browserCurrPath);
 	});
-	$(document).on("click", ".depot-browser-file", function()
+	$(document).on("click", ".browser-file", function()
 	{
+		$(".browser-upload-button").remove();
 		$("#filenameInput").val($(this).text());
-		isLocalFile = true;
+		$(this).parent().after("<tr class=\"browser-element browser-upload-button\"><th colspan=\"3\"><button class=\"depot-box uploadButton\" type=\"button\" style=\"width: 100%;\">Upload</button></th></tr>");
+		isRemoteFile = true;
+	});
+	$(document).on("click", ".browser-upload-all", function()
+	{
+		var dir = $(this).parent().parent().children(".browser-directory").text();
+		$.getJSON("/api/browse?path="+browserCurrPath+dir, function(data)
+		{
+			if(data.data.files.length > 0)
+			{
+				if(confirm("This directory has "+data.data.files.length+" of files. Are you sure?"))
+				{
+					$.each(data.data.files, function(_, fi)
+					{
+						console.log(fi.name);
+						isRemoteFile = true;
+						$("#filenameInput").val(fi.name);
+						upload(browserCurrPath+dir+"/"+fi.name);
+					});
+				}
+			}
+			else
+			{
+				alert("This directory doesn't contain any files");
+			}
+		});
+	});
+
+	$(document).on("click", ".link-container", function()
+	{
+		$(this).select();
 	});
 
 	var dropZone = document.getElementById('filenameInput');
@@ -124,31 +186,32 @@ $(function () {
 	dropZone.addEventListener('drop', handleFileSelect, false);
 });
 
-function showAlert(id, msg) {
-    $("#" + id + " > #alertText").text(msg);
-    
-    $("#" + id).animate({ bottom: 0 }, 500);
-    setTimeout(function () {
-        $("#" + id).animate({ bottom: -70 }, 300);
-    }, 2000);
+function timeUnitConverter(unit, value)
+{
+	var t = {"s": 1, "m": 60, "h": 3600, "d": 86400};
+	return value*t[unit];
 }
 
-function uploadLocalFile()
+function upload(path)
 {
-}
-
-function upload()
-{
-	if(isLocalFile)
+	var formData = new FormData($("#uploadForm")[0]);
+	if(isRemoteFile)
 	{
-		var formData = new FormData();
 		formData.append("local", 1);
-		console.log(browserCurrPath+$("#filenameInput").val());
-		formData.append("path", browserCurrPath+$("#filenameInput").val());
+		console.log(path);
+		formData.append("path", path);
 	}
-	else
-		var formData = new FormData($(".uploadForm")[0]);
 	var id = makeid();
+	
+	// Convert #expires_in to seconds
+	if($("#expires_in").val() !== "")
+	{
+		console.log(timeUnitConverter($("#expires_in").val(), $("#expires_in_unit").val()));
+		formData.append("expires_in", timeUnitConverter($("#expires_in_unit").val(), $("#expires_in").val()));
+	}
+
+	var filename = $("#filenameInput").val();
+	var fileext = filename.split('.').pop();
 
 	$.ajax({
 		url: "/upload",
@@ -169,7 +232,7 @@ function upload()
 		},
 		beforeSend: function ()
 		{
-			var item = $("<li class=\"list-group-item upload-list-item list_"+id+"\"><a class=\"a_"+id+"\">"+$("#filenameInput").val()+"</a> </li>");
+			var item = $("<li class=\"list-group-item upload-list-item list_"+id+"\"><a class=\"a_"+id+"\">"+filename+"</a> </li>");
 			var progbar = $('<div class="progress"><div id="progress_'+id+'" class="progress-bar" role="progressbar" style="width: 0%;"></div></div>');
 			item.append(progbar);
 			$("#uploadList").append(item);
@@ -179,11 +242,24 @@ function upload()
 		success: function(data)
 		{
 			var data = JSON.parse(data);
+			console.log(data);
 			var item = $(".list_"+id);
-			var fullurl = location.origin + "/" + data.path;
-			var link = "<a href=\"" + fullurl + "\">"+fullurl+"</a>";
-			$(".a_"+id).after(" - "+link);
-			$("#prgoress_"+id).css({ width: "100%" });
+			if(data.result)
+			{
+				var fullurl = location.origin + "/" + data.path;
+				var link = "<a href=\"" + fullurl + "\">"+fullurl+"</a>";
+				var s = " " + link;
+				s += "<div class=\"row\">";
+				s += "<div class=\"col-xs-12 col-sm-6\" style=\"display: inline-block\"><h4>Information</h4><input type=\"text\" value=\""+fullurl+"\" class=\"form-control link-container\" /></div>";
+				s += "<div class=\"col-xs-12 col-sm-6\" style=\"display: inline-block\"><h4>Direct download</h4><input type=\"text\" value=\""+fullurl+"."+fileext+"\" class=\"form-control link-container\" /></div>";
+				s += "</div>";
+				$(".a_"+id).after(s);
+				$("#prgoress_"+id).css({ width: "100%" });
+			}
+			else
+			{
+				$(".a_"+id).after(" - <span style=\"color: red\">Error: "+data.message+"</span>");
+			}
 		},
         	error: function (xhr, ajaxOptions, thrownError) {
 	        },
@@ -191,5 +267,7 @@ function upload()
         	cache: false,
 		contentType: false,
 		processData: false
-    });
+	});
+	$("#filenameInput").val("");
+	isRemoteFile = false;
 }
