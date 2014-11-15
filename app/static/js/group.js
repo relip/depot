f0466b108a5ae355b1f4339bda2aec3b3828f988
Larry Kim <admin@relip.org>
2014-11-15 16:32:02 +0900

    Implement groups page

---
+++ app/static/js/group.js
@@ -0,0 +1,21 @@
+$(function()
+{
+	$(document).on("click", ".edit-description", function()
+	{
+		$(this).parent().html('<input class="input description-editor form-control" type="text" data-path="'+$(this).attr("data-path")+'" value="'+$(this).parent().text()+'" />');
+		$(".description-editor").focus();
+	});
+	$(document).on("blur", ".description-editor", function()
+	{
+		console.log($(this).val());
+		$.post("/group/"+$(this).attr("data-path")+"/modify", {
+			description: $(this).val()
+		},
+		function(data)
+		{
+			console.log(data);
+		}, "json");
+		$(this).parent().html($(this).val()+'<span class="glyphicon glyphicon-pencil edit-description" data-path="'+$(this).attr("data-path")+'" aria-hidden="true"></span>');
+	});
+});
+
